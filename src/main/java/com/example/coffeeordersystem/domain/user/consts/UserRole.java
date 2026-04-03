2ad2a70de68e9b683aa74f98bf9ac633c122c8ed
Soomin <soomin02094@gmail.com>
2026-04-03 14:48:37 +0900

    feat: User 엔티티 추가

---
+++ src/main/java/com/example/coffeeordersystem/domain/user/consts/UserRole.java
@@ -0,0 +1,5 @@
+package com.example.coffeeordersystem.domain.user.consts;
+
+public enum UserRole {
+    ADMIN, USER
+}
---
+++ src/main/java/com/example/coffeeordersystem/domain/user/entity/User.java
@@ -0,0 +1,55 @@
+package com.example.coffeeordersystem.domain.user.entity;
+
+import com.example.coffeeordersystem.common.entity.BaseEntity;
+import com.example.coffeeordersystem.domain.user.consts.UserRole;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false, length = 50)
+    String name;
+
+    @Column(nullable = false, length = 100, unique = true)
+    String email;
+
+    @Column(nullable = false)
+    String password;
+
+    @Column(nullable = false)
+    int pointBalance;
+
+    @Column(nullable = false, length = 20)
+    @Enumerated(EnumType.STRING)
+    UserRole role;
+
+    LocalDateTime deletedAt;
+
+    public static User register(
+            String name,
+            String email,
+            String password,
+            UserRole role
+    ) {
+        User user = new User();
+
+        user.name = name;
+        user.email = email;
+        user.password = password;
+        user.pointBalance = 0;
+        user.role = role;
+
+        return user;
+    }
+}
