775508fa2a885a27c68c39c9da891a53b3f19f2f
Soomin <soomin02094@gmail.com>
2026-04-03 21:34:13 +0900

    feat: OrderStatus에 CANCELED 추가

--- src/main/java/com/example/coffeeordersystem/domain/order/consts/OrderStatus.java
+++ src/main/java/com/example/coffeeordersystem/domain/order/consts/OrderStatus.java
@@ -3,8 +3,9 @@ package com.example.coffeeordersystem.domain.order.consts;
 public enum OrderStatus {
     PENDING,    // 결제 대기
     PAID,       // 결제 완료
-    CONFIRMED,  // 주문 완료, 확인
+    CONFIRMED,  // 주문 확인
     PREPARING,  // 제조중
     READY,      // 제조 완료, 픽업 대기
-    COMPLETED   // 픽업 완료
+    COMPLETED,  // 픽업 완료
+    CANCELED    // 주문 취소
 }
